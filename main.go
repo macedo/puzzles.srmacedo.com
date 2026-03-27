674ab1a6d1d8f4d5111d1587a2cbc1b25efd8523
Rafael Macedo <macedo@87labs.com>
2026-03-27 20:00:46 -0300

    logging middleware

--- main.go
+++ main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
-	"log"
+	"context"
+	"crypto/rand"
+	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
+type ctxKey int
+
+const requestIDKey ctxKey = iota
+
 func main() {
+	var logger *slog.Logger
+	if os.Getenv("ENV") == "production" {
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	} else {
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+	slog.SetDefault(logger)
+
+	mux := http.NewServeMux()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 
 		if path == "/" {
@@ -48,15 +65,72 @@ func main() {
 		http.NotFound(w, r)
 	})
 
+	handler := loggingMiddleware(logger, mux)
+
 	server := http.Server{
 		Addr:         ":" + port,
-		Handler:      nil,
+		Handler:      handler,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 		IdleTimeout:  5 * time.Second,
 	}
 
 	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
+		logger.Error("Error starting server.... bye", "error", err)
+		os.Exit(1)
 	}
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+func generateRequestID() string {
+	b := make([]byte, 4)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+func loggingMiddleware(logger *slog.Logger, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		requestID := r.Header.Get("X-Request-ID")
+		if requestID == "" {
+			requestID = generateRequestID()
+		}
+
+		ctx := context.WithValue(r.Context(), requestIDKey, requestID)
+		r = r.WithContext(ctx)
+		w.Header().Set("X-Request-ID", requestID)
+
+		l := logger.With("request_id", requestID)
+
+		wrapped := newResponseWriter(w)
+
+		l.Info(
+			"Started",
+			"method", r.Method,
+			"path", r.URL.Path,
+			"ip", r.RemoteAddr,
+			"user_agent", r.UserAgent(),
+		)
+
+		next.ServeHTTP(wrapped, r)
+
+		l.Info(
+			"Completed",
+			"status", wrapped.statusCode,
+			"duration", time.Since(start),
+		)
+	})
 }
