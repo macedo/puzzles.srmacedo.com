2e97388e432b45fcb1bcf14b6be38f81dd72da53
Rafael Macedo <macedo@Rafaels-MacBook-Pro.local>
2026-03-27 14:47:06 -0300

    transorm site to pwa

---
+++ static/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "srmacedo-puzzles-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/sliding-puzzle.html",
+  "/css/application.css",
+  "/css/sliding-puzzle.css",
+  "/tetris.png"
+]
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
