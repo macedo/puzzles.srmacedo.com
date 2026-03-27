bf779668c4fa9aa7ec0a23d1bde5653dfa101a0b
Rafael Macedo <macedo@Rafaels-MacBook-Pro.local>
2026-03-27 08:49:45 -0300

    routing to better url

--- main.go
+++ main.go
@@ -8,14 +8,33 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
-
-	http.Handle("/", fs)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+
+		if path == "/" {
+			http.ServeFile(w, r, "./static/index.html")
+			return
+		}
+
+		fullPath := "./static/" + path + ".html"
+
+		info, err := os.Stat(fullPath)
+		if err == nil && !info.IsDir() {
+			http.ServeFile(w, r, fullPath)
+			return
+		}
+
+		if err != nil && !os.IsNotExist(err) {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+
+		http.NotFound(w, r)
+	})
 
 	server := http.Server{
 		Addr:         ":" + port,
